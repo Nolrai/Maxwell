d75daa5d5967a4d411e84c7d783ce15dc125a522
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2022-01-26 15:52:52 -0800

    LAB color glob

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 module Main (main, myImage) where
-import Maxwell (myPicture, okLAB)
+import Maxwell (myPicture, okLAB, okLAB', cieLAB')
 import Globe (inverseProjection)
 import Diagrams.Backend.SVG.CmdLine (mainWith)
 import Graphics.Image as Image
@@ -13,11 +13,12 @@ main =
     writeImage "sample.png" myImage
 
 myImage :: Image RPU Image.RGB Double
-myImage = makeImageR RPU (1080, 1920) (\ (x, y) -> imageFunction (fromIntegral x / 1080) (fromIntegral y / 1920))
+myImage = makeImageR RPU (18, 18) (\ (x, y) -> imageFunction (fromIntegral x / 18) (fromIntegral y / 18))
 
 imageFunction :: Double -> Double -> Pixel Image.RGB Double
-imageFunction x y = PixelRGB red green blue 
+imageFunction x y = let p = PixelRGB red green blue in Prelude.trace (show p) p
   where
-    Colour.RGB red green blue = toRGB $ okLAB l a b
-    l = l'/2 + 0.5
-    (V3 l' a b) = inverseProjection 1 (V2 (x * pi/2) (y * pi))
+    -- Colour.RGB red green blue = toRGB $ okLAB 0.3 (sin (x * 2 * pi)) (cos (y * 2 * pi))
+    Colour.RGB red green blue = toRGB $ cieLAB' 80 (50 * cos (x * 2 * pi)) (50 * cos (y * 2 * pi))
+    -- l = l'/2 + 0.5
+    -- (V3 l' a b) = inverseProjection 1 (V2 (x * pi/2) (y * pi))
